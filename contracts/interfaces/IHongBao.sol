20124c4effdfc29e25f8570a3fbbc5c554736e8e
pilagod <pilagooood@gmail.com>
2023-01-12 17:32:36 +0800

    add hongbao interface

---
+++ contracts/interfaces/IHongBao.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0;
+pragma abicoder v2;
+
+interface IHongBao {
+    struct Award {
+        string name;
+        uint16 count;
+        uint256 amount;
+    }
+
+    function createCampaign(
+        string calldata name,
+        address token,
+        uint256 expiry,
+        address[] calldata participants,
+        Award[] calldata awards
+    ) external returns (uint256 campaignId);
+
+    function closeCampaign(uint256 campaignId) external;
+
+    struct CampaignInfo {
+        address token;
+        uint256 remainingAwardAmount;
+        Award[] remainingAwards;
+    }
+
+    function getCampaignInfo(
+        uint256 campaignId
+    ) external view returns (CampaignInfo memory campaignInfo);
+
+    function draw(uint256 campaignId) external returns (uint256 amount);
+}
