10d4ebed7c8cefe298c1052e8b3d1b1b8b5f7010
pilagod <pilagooood@gmail.com>
2024-02-07 17:54:35 +0800

    upgrade solidity to 0.8

--- contracts/HongBao.sol
+++ contracts/HongBao.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.7.6;
-pragma abicoder v2;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "./interfaces/IHongBao.sol";
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -44,7 +44,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.7.6",
+                version: "0.8.17",
                 settings: {
                     optimizer: {
                         enabled: true,
