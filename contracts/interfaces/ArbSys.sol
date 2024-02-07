716737a8ceee8aca9311d371405f2dc11f257f4a
pilagod <pilagooood@gmail.com>
2024-02-07 12:20:16 +0800

    add arbsys interface

---
+++ contracts/interfaces/ArbSys.sol
@@ -0,0 +1,13 @@
+// Copyright 2021-2022, Offchain Labs, Inc.
+// For license information, see https://github.com/OffchainLabs/nitro-contracts/blob/main/LICENSE
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity >=0.4.21 <0.9.0;
+
+interface ArbSys {
+    /**
+     * @notice Get Arbitrum block number (distinct from L1 block number; Arbitrum genesis block has block number 0)
+     * @return block number as int
+     */
+    function arbBlockNumber() external view returns (uint256);
+}
